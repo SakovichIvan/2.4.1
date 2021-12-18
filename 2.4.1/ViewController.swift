eba7c1313e0dfd533a13dd34e572008490555f27
Ivan <cudik.cudikov@mail.ru>
2021-12-18 17:45:42 +0300

    Initial Commit

---
+++ 2.4.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.4.1
+//
+//  Created by Ivan on 18.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
